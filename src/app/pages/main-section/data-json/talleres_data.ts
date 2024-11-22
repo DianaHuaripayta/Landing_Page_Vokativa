1e9ef92468bde1a51edf28873d88c957685e708e
DianaHuaripayta <angelicagonzales1610@gmail.com>
2024-11-22 11:20:28 -0500

    demo deploy - v1

--- src/app/pages/main-section/data-json/talleres_data.ts
+++ src/app/pages/main-section/data-json/talleres_data.ts
@@ -5,21 +5,21 @@ export const TALLERES = [
     nombre: 'Taller de Ingeniería',
     precio: 'S/. 25.00',
     fecha: 'Viernes 15 Noviembre',
-    temas: ['Ingeniería Civil', 'Ingeniería de Sistemas', 'Ingeniería Industrial', 'Ingeniería Ambiental'],
+    temas: ['Ingeniería Civil', 'Ingeniería Industrial', 'Ingeniería Ambiental'],
     imagen: `${environment.assetsBasePath}ingeneria.jpg`
   },
   {
     nombre: 'Taller de Ciencias Empresariales',
     precio: 'S/. 25.00',
     fecha: 'Sábado 16 Noviembre',
-    temas: ['Administración', 'Contabilidad', 'Economía', 'Marketing'],      
+    temas: ['Administración de empresas', 'Contabilidad', 'Economía', 'Marketing'],      
     imagen: `${environment.assetsBasePath} cienciasEmpresariales.jpg`
   },
   {
     nombre: 'Taller de Tecnología',
     precio: 'S/. 25.00',
     fecha: 'Domingo 17 Noviembre',
-    temas: ['Ingeniería de Software', 'Redes y Telecomunicaciones', 'Inteligencia Artificial y Ciencia de Datos'],
+    temas: ['Ingeniería de Sistemas','Ingeniería de Software', 'Redes y Telecomunicaciones'],
     imagen: `${environment.assetsBasePath}Tecnologia.jpg`
   },
   {
