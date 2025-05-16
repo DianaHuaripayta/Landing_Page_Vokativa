2bbbaa4dc3c6b1ccdf45d2e8fedf786863538f55
DianaHuaripayta <angelicagonzales1610@gmail.com>
2025-05-16 17:01:09 -0500

    fix Routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import WelcomeScreen from "./pages/welcome/WelcomeScreen";
 import Layout from './layout/Layout'
+import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
@@ -10,6 +11,17 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/home" element={<Home />} />
       </Route>
+      {/* Ruta para manejar redirect de GitHub Pages */}
+        <Route 
+          path="/vokativa" 
+          element={<Navigate to="/" replace />} 
+        />
+        <Route 
+          path="/vokativa/*" 
+          element={<Navigate to="/" replace />} 
+        />
+      {/* Ruta comodín para el error 404 */}
+        <Route path="*" element={<NotFound />} />
     </Routes>
   </>    
   );
