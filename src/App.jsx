69b06ad694c164eb1f7fc33a315690e5ba2bc5d6
DianaHuaripayta <angelicagonzales1610@gmail.com>
2025-05-16 17:12:35 -0500

    add import Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route , Navigate} from "react-router-dom";
 import Home from "./pages/Home/Home";
 import WelcomeScreen from "./pages/welcome/WelcomeScreen";
 import Layout from './layout/Layout'
