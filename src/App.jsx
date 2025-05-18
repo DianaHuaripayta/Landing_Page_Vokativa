f37d598c7265fdc8a8a2b8974328a4819a458145
DianaHuaripayta <angelicagonzales1610@gmail.com>
2025-05-17 19:10:11 -0500

    add 404.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route , Navigate} from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import WelcomeScreen from "./pages/welcome/WelcomeScreen";
 import Layout from './layout/Layout'
@@ -6,23 +6,15 @@ import NotFound from "./pages/NotFound";
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<WelcomeScreen />} />
-      <Route element={<Layout />}>
-        <Route path="/home" element={<Home />} />
-      </Route>
-      {/* Ruta para manejar redirect de GitHub Pages */}
-        <Route 
-          path="/vokativa" 
-          element={<Navigate to="/" replace />} 
-        />
-        <Route 
-          path="/vokativa/*" 
-          element={<Navigate to="/" replace />} 
-        />
-      {/* Ruta comodín para el error 404 */}
-        <Route path="*" element={<NotFound />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Navigate to="/welcome" replace />} />
+          <Route path="/welcome" element={<WelcomeScreen />} />
+        <Route path="/" element={<Layout />}>
+        
+          <Route path="/home" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
   </>    
   );
 }
