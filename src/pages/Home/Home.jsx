b887a38c4fccf5d0abc603ed92b603eadd7f30a4
DianaHuaripayta <angelicagonzales1610@gmail.com>
2025-07-02 13:26:58 -0500

    new: customzed Dialog (form)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "../../sections/hero/Hero";
 import About from "../../sections/about/About";
 import ImmersiveExperience from "../../sections/immersiveExperience/ImmersiveExperience";
@@ -7,9 +7,32 @@ import OurGraduates from "../../sections/ourGraduates/OurGraduates";
 import OurWorkshop from "../../sections/OurWorkshop/OurWorkshop";
 import Impact from "../../sections/impact/Impact";
 import ImpactMessage from "../../sections/impactMessage/ImpactMessage";
+import CustomizedDialogs from "../../Components/modals/CustomizedDialogs";
+import formImage from "../../assets/posts/form.jpeg"; // Import the image
+
 const Home = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modalText, setModalText] = useState("");
+  const handleShowModal = () => {
+    setModalText(
+      "Haz clic en el botón de abajo 👇 para acceder al formulario de inscripción y ser parte de nuestra comunidad Vokativa 🙌. ¡Te esperamos👩‍💻!"
+    );
+    setModalOpen(true);
+  };
+  useEffect(() => {
+    handleShowModal();
+  }, []);
   return (
     <>
+      <CustomizedDialogs
+        open={modalOpen}
+        onClose={() => setModalOpen(false)}
+        content={modalText}
+        url={formImage}
+        btnCloseText="Ir a formulario de inscripción"
+          redirectUrl="https://docs.google.com/forms/d/e/1FAIpQLSdmDVQpcDWuM-EksIMDIfXAfH1atw7WP7gwfN9UU1KeOwkMyA/viewform" // Si no quieres redirigir, pasa null o no pongas esta prop
+
+      />
       <div id="hero">
         <Hero />
       </div>
--- src/pages/welcome/WelcomeScreen.jsx
+++ src/pages/welcome/WelcomeScreen.jsx
@@ -5,12 +5,14 @@ import V3D from "../../components/3D/V3D";
 import Slogan3D from "../../components/3D/Slogan3D";
 import SpringyBoxes from "../../Components/3D/SpringyBoxes/SpringyBoxes";
 import styles from "./Welcome.module.css";
-
+import CustomizedDialogs from "../../Components/modals/CustomizedDialogs";
 function WelcomeScreen() {
   const navigate = useNavigate();
   const [showWelcome, setShowWelcome] = useState(false);
   const [hideWelcome, setHideWelcome] = useState(false);
-  const [showBoxes, setShowBoxes] = useState(false);
+
+  
+//  const [showBoxes, setShowBoxes] = useState(false);
 
   useEffect(() => {
     const welcomeTimer = setTimeout(() => {
@@ -18,21 +20,23 @@ function WelcomeScreen() {
       const hideTimer = setTimeout(() => {
         setHideWelcome(true);
           navigate("/home");
+           
+
       }, 2000);
       return () => clearTimeout(hideTimer);
-    }, 4000);
+    }, 1000);
 
-    const boxTimer = setTimeout(() => {
-      setShowBoxes(true);
-      const hideBoxTimer = setTimeout(() => {
-        setShowBoxes(false);
-      }, 3000);
-      return () => clearTimeout(hideBoxTimer);
-    }, 3000);
+    // const boxTimer = setTimeout(() => {
+    //   setShowBoxes(true);
+    //   const hideBoxTimer = setTimeout(() => {
+    //     setShowBoxes(false);
+    //   }, 3000);
+    //   return () => clearTimeout(hideBoxTimer);
+    // }, 3000);
 
     return () => {
       clearTimeout(welcomeTimer);
-      clearTimeout(boxTimer);
+    // clearTimeout(boxTimer);
     };
   }, [navigate]);
 
@@ -41,13 +45,13 @@ function WelcomeScreen() {
       <Canvas shadows camera={{ position: [0, 0, 80], fov: 10 }}>
         <V3D />
         <Slogan3D />
-        {showBoxes && <SpringyBoxes />}
+        {/* {showBoxes && <SpringyBoxes />} */}
       </Canvas>
-      {showWelcome && !hideWelcome && (
+      {/* {showWelcome && !hideWelcome && (
         <div className={`${styles.welcome_screen} ${styles.fade_in_out}`}>
           <h1 className={styles.paragraph}>¡Bienvenido a la experiencia VOKATIVA!</h1>
         </div>
-      )}
+      )} */}
     </div>
   );
 }
